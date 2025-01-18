81fa5223227a1fcbc19dcad0a1ac87f8426696f1
RameshLodh <rahulrajput81412@gmail.com>
2025-01-18 17:09:19 +0530

    adde ["shipping.local", "localhost", "https://shipping.local"]

--- shipping_managment_system/settings.py
+++ shipping_managment_system/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["shipping.local", "localhost", "https://shipping.local"]
 
+CSRF_TRUSTED_ORIGINS = ["shipping.local", "localhost", "https://shipping.local"]
 
 # Application definition
 
